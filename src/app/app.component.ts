081a2c79d55ecc4082641eec3654ddfc85d1d4d5
AnnaVyvert <0073av@gmail.com>
2023-07-11 19:18:20 +0800

    feat: up scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,57 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+interface JsonScheme {
+  id: number;
+  name: string;
+  pic_url: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+  localStoreName: string = "json"
 
-  jsonLocalStoreName: string = "json"
+  cards: JsonScheme[] = this.loadJson()[2]
 
-  // cards = Array(5).map((e, i)=> {
-  //   e.title = 'Card '+i,
-  //   e.image = 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //   e.description = 'Description for Card '+i,
-  //   e.footer = 'Footer for Card '+i
-  // })
-  // cards = [
-  //   {
-  //     title: 'Card 1',
-  //     image: 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //     description: 'Description for Card 1',
-  //     footer: 'Footer for Card 1'
-  //   },
-  //   {
-  //     title: 'Card 2',
-  //     image: 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //     description: 'Description for Card 2',
-  //     footer: 'Footer for Card 2'
-  //   },
-  //   {
-  //     title: 'Card 3',
-  //     image: 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //     description: 'Description for Card 2',
-  //     footer: 'Footer for Card 2'
-  //   },
-  //   {
-  //     title: 'Card 4',
-  //     image: 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //     description: 'Description for Card 2',
-  //     footer: 'Footer for Card 2'
-  //   },
-  //   {
-  //     title: 'Card 5',
-  //     image: 'https://i0.wp.com/www.flutterbeads.com/wp-content/uploads/2022/01/add-image-in-flutter-hero.png?fit=2850%2C1801&ssl=1',
-  //     description: 'Description for Card 2',
-  //     footer: 'Footer for Card 2'
-  //   },
-  // ];
-
-  cards = this.loadJson()
-
-  displayedCards: { title: string; name: string; pic_url: string; description: string; footer: string; }[] = [];
-  batchSize = 3; // Number of cards to load at a time
+  displayedCards: JsonScheme[] = [];
+  batchSize = this.cards.length; // Number of cards to load at a time
   // @ts-ignore
   cardTitle: string = this.cards[0].name 
 
   ngOnInit() {
     // Simulating initial data loading
-    this.loadMoreCards();
+    this.loadCardsDown();
+
   }
 
-  loadMoreCards() {
+  ngAfterViewInit() {
+    console.log(this.scrollContainer.nativeElement.scrollTop);
+    
+    this.loadCardsUp()
+  }
+
+  loadCardsUp() {
+    const startIndex = this.displayedCards.length % this.cards.length;
+    const endIndex = startIndex + this.batchSize;
+  
+    if (this.displayedCards.length === 0 || this.displayedCards.length < this.cards.length) {
+      // Load cards normally
+      
+      const newCards = this.cards.slice(startIndex, endIndex);
+      this.displayedCards = [...newCards, ...this.displayedCards];
+    } else {
+      // Wrap around and load cards from the beginning
+      const wrappedCards = this.cards.slice(startIndex).concat(this.cards.slice(0, this.batchSize));
+      this.displayedCards = [...wrappedCards, ...this.displayedCards];
+    }
+    this.scrollContainer.nativeElement.scrollTop += this.batchSize*297;
+  }
+
+  loadCardsDown() {
     const startIndex = this.displayedCards.length % this.cards.length;
     const endIndex = startIndex + this.batchSize;
   
@@ -70,25 +59,33 @@ export class AppComponent {
       // Load cards normally
       const newCards = this.cards.slice(startIndex, endIndex);
       this.displayedCards = this.displayedCards.concat(newCards);
+      console.error(newCards.length);
     } else {
       // Wrap around and load cards from the beginning
       const wrappedCards = this.cards.slice(startIndex).concat(this.cards.slice(0, this.batchSize));
       this.displayedCards = this.displayedCards.concat(wrappedCards);
+      console.error(wrappedCards.length);
     }
   }
   
-  
-  
-
   onScroll() {
     const container = this.scrollContainer.nativeElement;
-    // const isScrolledToTop = container.scrollTop + container.clientHeight === 0;
-    const isScrolledToBottom = container.scrollTop + container.clientHeight >= container.scrollHeight;
-    console.log(container.scrollTop + container.clientHeight, container.scrollHeight);
+    const isScrolledToTop = container.scrollTop <= 0 + 500;
+    const isScrolledToBottom = container.scrollTop + container.clientHeight >= container.scrollHeight - 300;
     
     if (isScrolledToBottom) {
-      this.loadMoreCards();
+      this.loadCardsDown();
+      console.log(container.scrollTop);
+    } else if (isScrolledToTop) {
+      this.loadCardsUp();
+      // container.scrollTop = container.scrollTop+(292+20)*this.batchSize
+      // container.scrollTop += 3206
+      // console.log(container.scrollTop);
+      
     }
+    // console.log(this.cards.length);
+    // container.scrollTop = container.scrollTop+10
+    
 
     const intersectionOptions = {
       root: null,
@@ -113,9 +110,9 @@ export class AppComponent {
 
 
 
-  loadJson(): [] {
+  loadJson(): JsonScheme[][] {
     return JSON.parse(
-      window.localStorage.getItem(this.jsonLocalStoreName) ?? '[]'
+      window.localStorage.getItem(this.localStoreName) ?? '[]'
     );
   }
 
