d0260ef6d849c3745ac5279d747964bf9c7af092
AnnaVyvert <0073av@gmail.com>
2023-07-13 20:57:37 +0800

    feat: visibility controller page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScrollPageComponent } from './components/scroll-page/scroll-page.component';
+import { SwitchVisibilityPageComponent } from './components/switch-visibility-page/switch-visibility-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScrollPageComponent
+    ScrollPageComponent,
+    SwitchVisibilityPageComponent
   ],
   imports: [
     BrowserModule,
