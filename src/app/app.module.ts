30021cda780032fab74720fb95227baa550964a6
AnnaVyvert <0073av@gmail.com>
2023-07-13 20:28:21 +0800

    feat: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScrollPageComponent } from './components/scroll-page/scroll-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
