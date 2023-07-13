ebf96955febf6ac26b271ba00e84bbb8508d8e2b
AnnaVyvert <0073av@gmail.com>
2023-07-13 23:22:34 +0800

    feat(scroll-page)

---
+++ src/app/components/scroll-page/scroll-page.component.ts
@@ -0,0 +1,91 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { getRandomInt } from 'src/utils/rand';
+import { JsonScheme } from 'src/interfaces/jsonScheme';
+import { loadStore, setValue2Store } from 'src/utils/json-worker';
+import { scrollDown, scrollUp } from 'src/utils/scroll';
+
+@Component({
+  selector: 'app-scroll-page',
+  templateUrl: './scroll-page.component.html',
+  styleUrls: ['./scroll-page.component.scss']
+})
+export class ScrollPageComponent {
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+
+  scrollElement: HTMLElement | undefined;
+
+  store: JsonScheme[][] = loadStore('json');
+  cards: JsonScheme[] = this.store[0];
+
+  displayedCards: JsonScheme[] = [];
+
+  cardTitle: string = this.cards[0].name;
+
+  ngOnInit(): void {
+    this.displayedCards = scrollDown(this.cards, this.displayedCards);
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollElement = this.scrollContainer.nativeElement;
+    this.scrollStartInMiddle()
+  }
+
+  scrollStartInMiddle = () => 
+    this.scrollElement!.scrollTop = this.scrollElement!.clientHeight / 2;
+
+  updateCards(i: number) {
+    setValue2Store('selected-json', i.toString());
+    this.cards = this.store[i]
+    this.displayedCards = this.cards;
+    this.scrollStartInMiddle()
+  }
+
+  isScroll = false;
+  interval: undefined | ReturnType<typeof setInterval>;
+
+  continiusScroll(): void {
+    this.isScroll = !this.isScroll;
+    if (this.isScroll) {
+      this.interval = setInterval(() => {
+        this.scrollContainer.nativeElement.scrollTop += 20;
+      }, 10);
+    } else {
+      clearInterval(this.interval);
+    }
+  }
+
+  randomScroll(): void {
+    let scrollPower = getRandomInt(-60, -10);
+    const powerLimit = getRandomInt(80, 140);
+    const interval = setInterval(() => {
+      this.scrollContainer.nativeElement.scrollTop += scrollPower;
+      scrollPower++;
+      if (scrollPower > powerLimit) {
+        clearInterval(interval);
+        const interval2 = setInterval(() => {
+          this.scrollContainer.nativeElement.scrollTop += scrollPower;
+          scrollPower--;
+          if (scrollPower < 1) clearInterval(interval2);
+          console.log(interval, interval2);
+        }, 15);
+      }
+    }, 15);
+  }
+
+  onScroll(): void {
+    const isScrolledToTop = this.scrollElement!.scrollTop <= 0 + 500;
+    const isScrolledToBottom =
+      this.scrollElement!.scrollTop + this.scrollElement!.clientHeight >=
+      this.scrollElement!.scrollHeight - 300;
+
+    if (isScrolledToBottom) {
+      this.displayedCards = scrollDown(this.cards, this.displayedCards);
+    } else if (isScrolledToTop) {
+      this.displayedCards = scrollUp(
+        this.cards,
+        this.displayedCards,
+        this.scrollElement!
+      );
+    }
+  }
+}
