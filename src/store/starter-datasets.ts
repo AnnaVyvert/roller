438f1b0b609cfe714ceffe1020309dd507fede23
AnnaVyvert <0073av@gmail.com>
2023-07-16 01:30:53 +0800

    feat: starter file

---
+++ src/store/starter-datasets.ts
@@ -0,0 +1,192 @@
+import { JsonScheme } from 'src/interfaces/jsonScheme';
+
+export const starter: JsonScheme[][] = [
+  [
+    {
+      id: 0.9527044988127846,
+      name: 'омлет',
+      pic_url:
+        'https://lobsterhouse.ru/wp-content/uploads/2/b/1/2b1752da4b27ac0b473ece5f28c8710a.jpeg',
+    },
+    {
+      id: 0.10977464260317915,
+      name: 'бутерброды',
+      pic_url:
+        'https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663817812_53-mykaleidoscope-ru-p-buterbrodi-s-vetchinoi-na-prazdnichnii-sto-66.jpg',
+    },
+    {
+      id: 0.30601473229908605,
+      name: 'яичница',
+      pic_url:
+        'https://ketokotleta.ru/wp-content/uploads/6/b/7/6b74d73c2386204df5da7757546d7e27.jpeg',
+    },
+    {
+      id: 0.12801783649982523,
+      name: 'тосты',
+      pic_url: 'https://i.ytimg.com/vi/HUQO_tQ98tE/maxresdefault.jpg',
+    },
+    {
+      id: 0.0513593422782046,
+      name: 'гренки',
+      pic_url:
+        'https://pic.rutubelist.ru/video/de/22/de22a57e0025bb292010f78d1a93e117.jpg',
+    },
+    {
+      id: 0.39297810638166375,
+      name: 'каша рисовая',
+      pic_url:
+        'http://static.tildacdn.com/tild6466-3939-4631-a536-613164623734/_.jpg',
+    },
+    {
+      id: 0.06818288229179625,
+      name: 'каша манная',
+      pic_url: 'https://ampravda.ru/files/articles-2/64510/zdrb6yv01es2.jpg',
+    },
+    {
+      id: 0.7640610590895276,
+      name: 'блины',
+      pic_url:
+        'https://montisbar.ru/wp-content/uploads/e/0/e/e0e9db613f8e2f1e06304af804419ec8.jpeg',
+    },
+    {
+      id: 0.05144010473790228,
+      name: 'оладушки',
+      pic_url:
+        'https://lobsterhouse.ru/wp-content/uploads/b/2/3/b233217c809712eee9acf603de85318d.jpeg',
+    },
+    {
+      id: 0.7042086696998202,
+      name: 'творог',
+      pic_url:
+        'https://domstrousam.ru/wp-content/uploads/2021/04/tvorog_kisliy.jpg',
+    },
+  ],
+  [
+    {
+      id: 0.9985326592789099,
+      name: 'суп солянка',
+      pic_url:
+        'https://pigmine.ru/wp-content/uploads/4/c/e/4cea48daf333c2b7950f3f73e8f4200e.jpeg',
+    },
+    {
+      id: 0.6501679341635551,
+      name: 'суп щи',
+      pic_url:
+        'https://pigmine.ru/wp-content/uploads/c/7/d/c7d3eeee17b48fc8520e164652a53f8d.jpeg',
+    },
+    {
+      id: 0.37690928279287594,
+      name: 'суп куриный',
+      pic_url:
+        'https://kvashenaya-kapusta.ru/wp-content/uploads/0/2/6/026f34d7dbc4fd4ecced3d1ab720b32e.jpeg',
+    },
+    {
+      id: 0.8054221906859498,
+      name: 'суп борщ',
+      pic_url:
+        'https://montisbar.ru/wp-content/uploads/4/7/2/47296d35b014f9f9d85ea5f95694e6b2.jpeg',
+    },
+    {
+      id: 0.3694910932602409,
+      name: 'суп томатный',
+      pic_url:
+        'https://sun6-23.userapi.com/impg/D-C3TAmgNsi8ArPghHFrDjhkFgQtYNXpOitJYw/2Cqy2MJ4Qy4.jpg?size=974x649&quality=96&sign=8b8c07406a869a27d5e63bb7b366a659&c_uniq_tag=05SSU7ENKXIdPSf5BRajnabmfFfU-TvhyPFUzEXXoRA&type=album',
+    },
+    {
+      id: 0.5418328168217919,
+      name: 'суп бухлёр',
+      pic_url:
+        'https://static.tildacdn.com/tild3136-6161-4735-b561-363234383864/pic_5a2621f1c914f.jpg',
+    },
+    {
+      id: 0.6011301641132822,
+      name: 'суп рамён',
+      pic_url:
+        'https://elenaportnova.ru/wp-content/uploads/3/5/2/352f67ce6b0e82b2341f9947b53c8c95.jpeg',
+    },
+    {
+      id: 0.049625292365953255,
+      name: 'суп сливочный',
+      pic_url: 'https://profile.ru/wp-content/uploads/1191442-fb.jpeg',
+    },
+  ],
+  [
+    {
+      id: 0.8129797304439883,
+      name: 'спагетти с томатами',
+      pic_url: 'https://fb.ru/misc/i/gallery/102881/2747093.jpg',
+    },
+    {
+      id: 0.9207653063866041,
+      name: 'карбонара',
+      pic_url:
+        'https://yanva.ru/wp-content/uploads/2020/07/3243242-6-scaled.jpg',
+    },
+    {
+      id: 0.5475736518963172,
+      name: 'пюре со свининой',
+      pic_url:
+        'https://i.pinimg.com/originals/fb/93/73/fb9373095af5f03619d26358de347154.jpg',
+    },
+    {
+      id: 0.022594140060435253,
+      name: 'пюре с запечённой курицей',
+      pic_url:
+        'https://pics.botanichka.ru/wp-content/uploads/2018/06/kurinyie-okorochka-na-skovorode-02.jpg',
+    },
+    {
+      id: 0.6485759243007934,
+      name: 'спагетти с курицей и грибами',
+      pic_url:
+        'https://kartinkin.net/uploads/posts/2021-07/1625650824_24-kartinkin-com-p-spagetti-s-gribami-v-slivochnom-souse-yeda-30.jpg',
+    },
+    {
+      id: 0.9737308446921551,
+      name: 'курица с макаронами',
+      pic_url:
+        'https://pizza-top.ru/800/600/https/izkuricy.ru/wp-content/uploads/2018/12/Grilled_Chicken_Spaghetti_Pasta_Recipe_with_Herbs-1.jpg',
+    },
+    {
+      id: 0.2188902382156479,
+      name: 'рис с курицей терияки',
+      pic_url:
+        'https://www.vsegdavkusno.ru/assets/images/recipes/2390/big/20-minut-dlja-vkusnogo-uzhina-kurica-terijaki-s-risom-i-sous-pokupat-ne-nuzhno-low-1.jpg',
+    },
+    {
+      id: 0.7891556077104749,
+      name: 'рис с кислосладкой курицей',
+      pic_url:
+        'https://i.mycdn.me/i?r=AyH4iRPQ2q0otWIFepML2LxRzOGOOYS4L5f76cKnafExJg',
+    },
+    {
+      id: 0.7488620643166501,
+      name: 'говядина тушёная с овощами и рисом',
+      pic_url:
+        'https://uzbek-culinar.ru/wp-content/uploads/7/c/1/7c150cd96e06e5d84e5e282c8143018c.jpeg',
+    },
+    {
+      id: 0.1762642081246979,
+      name: 'пицца',
+      pic_url:
+        'https://hobbyndom.ru/wp-content/uploads/4/6/4/4642e6d96e9cb6eb1bd8d64e9b552b22.jpeg',
+    },
+    {
+      id: 0.09593195195427295,
+      name: 'гречка с печёнкой',
+      pic_url:
+        'https://beautylunch.ru/wp-content/uploads/2021/07/main_b_grechka-s-kurinoy-pechenyu.jpg',
+    },
+    {
+      id: 0.3367205130090529,
+      name: 'рыба с картошкой',
+      pic_url:
+        'https://yandex-images.clstorage.net/bwK4G9198/2daeb0K6bVUI/fTuSeVErZpe8c_LmESxc0dBuqjmZg3dqEPpXKG1iMIOtwOe3RoSLa0jHL9zThqjSHuXNF0jT4dzEHr8Xb_mRfEiLs8wwDhMCOMfjUhRtXWAAWxoZ7RIH78VeBlhsUhCivaDHNpcUCaxPwFiapHCf1Jzn_PONpjGYMNrOccjYFXv670T7k0mVV0zDtOQw_hoCskzquLlHBNrB_Le6jvRQ0z-whucHNXndDIDL-EchPiTdFl1DXzZg-2U67_IZ_7dpml3XqSILFSb-UGRHYn4I0xLMypno1uVYo-5m-s71ksGtcffUtnVYv1xyuTmF0S1k3jUvs6zWgzr1S1xBe40lm1nPx8rDOwclTND3BZP_ynHB7okoqae1GIOeZklOBEIyHrM0xQfW2Hwv4-mZBeDv1qylbiIOdAD750k_AXqvFMoYviba4upnhM0xlPdj_5jzIVwauOj1hblTjBT7LKTCwD3St6a3FiktLPCqiYYBrNWvBF5h_ETS6fQZP4KprqUbql0myxKYZyatYfZE8A94cPAtW2q49AarwQ31iPxFcDPf0hT0tscbTY2QOuglot0n3MYN4B-H8lnEm3-iq5y3ODgcN6jBODWEPEG2leOd6OFiLer7e1U2iTKfhXietCMCvXI3pqckKE2NQQg5BREvFH4kLwM9NjPqhxkdAVtOt1l4_GUII3llBt3iZBTgfRmgMryqmzqHJqnTruTJHDaAwQ2iZ8akpLjdzrILO-Uiz4Rf5S5zT6azmLVZb2MJvFT4Cr8UeAKIVpUuQaaU45xJssJMCijbJtdYcR7F6L0EYRA-0SUHpmSqDQ_CivkWQY013jQ8QU9UAeqXCm2janyWe4u8VipgCWTm3dGFlKF_CiLR_ygp64YGiJKtxPkelABxTpKX9Bb0O92PohrYV6F9lW6mfsFdFKELBErf4hl-5qtbjATJkio15PzTlLWhHxsAI97KKbpEpSgSvwZpE',
+    },
+    {
+      id: 0.8758723356379772,
+      name: 'макароны с тефтелями',
+      pic_url:
+        'https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-9c3da611-534e-49db-82aa-77abd5f7862c',
+    },
+  ],
+];
